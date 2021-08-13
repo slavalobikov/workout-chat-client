b85199491907ede8e792d9170db734376ab20479
slava <lobikova12@mail.ru>
2021-08-13 15:53:40 +0300

    attachments

---
+++ src/components/Attachments/index.js
@@ -0,0 +1,27 @@
+import cn from 'classnames';
+
+import "./Attachments.scss";
+
+/*
+Сделать так, что бы, когда была одна фотка, она была больше!
+
+5.33  https://www.youtube.com/watch?v=oS3vO-kuPgE
+*/
+
+const Attachments = ({attachments, isMe}) => {
+    return (
+        <div className={cn("attachments__photos",
+            {
+                "attachments__photos-isMe": isMe,
+            }
+        )}>
+            {
+                attachments.map((f, item) => <div key={item}>
+                    {f.filename === 'image.jpg' && <img src={f.url} alt="Attachments file"/>}
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default Attachments;
